03fcbb3edc4be4f452a1df194b5a70a014d6a07b
musharrafz <meibrohimov@gmail.com>
2023-05-17 10:37:02 +0500

    full change

--- apps/admin.py
+++ apps/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-
 from apps.models import Document
 
-admin.site.register(Document)
+
+@admin.register(Document)
+class DocumentModelAdmin(admin.ModelAdmin):
+    change_form_template = 'apps/view.html'
+    list_display = ('name',)
+
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['image_url'] = Document.objects.get(pk=object_id).picture.url.replace('minio', 'localhost', 1)
+        extra_context['file_url'] = Document.objects.get(pk=object_id).pdf.url.replace('minio', 'localhost', 1)
+        return super().change_view(request, object_id, form_url, extra_context)
--- apps/models.py
+++ apps/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class Document(models.Model):
     name = models.CharField(max_length=222)
-    image = models.ImageField(upload_to='images/', validators=[FileExtensionValidator(['jpg', 'jpeg'])])
+    picture = models.ImageField(upload_to='images/', validators=[FileExtensionValidator(['jpg', 'jpeg'])])
     pdf = models.FileField(upload_to='pdf/', validators=[FileExtensionValidator(['pdf'])])
